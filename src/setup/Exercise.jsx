4db5ecb83623c40e1bb3166e7c4e2047d6797eab
UrNas <qais88@yahoo.com>
2019-02-02 20:04:52 +0300

    revert String with single quotesand indentation with 4 spaces

--- src/setup/Exercise.jsx
+++ src/setup/Exercise.jsx
@@ -1,39 +1,39 @@
-import React from "react";
-import styled from "styled-components";
-import Solution from "./Solution";
+import React from 'react';
+import styled from 'styled-components';
+import Solution from './Solution';
 
 const Container = styled.div`
-  display: flex;
+    display: flex;
 `;
 
 const SolutionContainer = styled.div`
-  margin: 5px;
-  width: 50%;
-  min-height: 100px;
+    margin: 5px;
+    width: 50%;
+    min-height: 100px;
 `;
 
 const SolutionTitle = styled.div`
-  padding-bottom: 10px;
-  font-size: 0.9rem;
+    padding-bottom: 10px;
+    font-size: 0.9rem;
 `;
 
 const Exercise = props => {
-  const {
-    exercise: { location, solutionLocation }
-  } = props;
-  return (
-    <Container>
-      <SolutionContainer>
-        <SolutionTitle>Your Solution: ({location})</SolutionTitle>
-        <Solution location={location} />
-      </SolutionContainer>
-      <div style={{ margin: 5 }} />
-      <SolutionContainer>
-        <SolutionTitle>Target Solution: ({solutionLocation})</SolutionTitle>
-        <Solution location={solutionLocation} finalSolution />
-      </SolutionContainer>
-    </Container>
-  );
+    const {
+      exercise: { location, solutionLocation }
+    } = props;
+    return (
+      <Container>
+        <SolutionContainer>
+          <SolutionTitle>Your Solution: ({location})</SolutionTitle>
+          <Solution location={location} />
+        </SolutionContainer>
+        <div style={{ margin: 5 }} />
+        <SolutionContainer>
+          <SolutionTitle>Target Solution: ({solutionLocation})</SolutionTitle>
+          <Solution location={solutionLocation} finalSolution />
+        </SolutionContainer>
+      </Container>
+    );
 };
 
 export default Exercise;
--- src/setup/TutorialNavigation.jsx
+++ src/setup/TutorialNavigation.jsx
@@ -1,49 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Container = styled.div`
-  display: table;
-  text-align: center;
-  width: 100%;
+    display: table;
+    text-align: center;
+    width: 100%;
 `;
 
 const Navigation = styled.div`
-  width: 33%;
-  display: table-cell;
+    width: 33%;
+    display: table-cell;
 `;
 
 const linkStyle = {
-  padding: 10,
-  backgroundColor: "#3f51b5",
-  color: "white",
-  borderRadius: 5
+    padding: 10,
+    backgroundColor: "#3f51b5",
+    color: "white",
+    borderRadius: 5
 };
 
 const TutorialNavigation = ({ previousTutorial, nextTutorial }) => {
-  return (
-    <Container>
-      <Navigation>
-        {previousTutorial && (
-          <Link style={linkStyle} to={`${previousTutorial.route}`}>
-            ← {previousTutorial.displayName}
+    return (
+      <Container>
+        <Navigation>
+          {previousTutorial && (
+            <Link style={linkStyle} to={`${previousTutorial.route}`}>
+              ← {previousTutorial.displayName}
+            </Link>
+          )}
+        </Navigation>
+        <Navigation>
+          <Link style={linkStyle} to="/">
+            Home
           </Link>
-        )}
-      </Navigation>
-      <Navigation>
-        <Link style={linkStyle} to="/">
-          Home
-        </Link>
-      </Navigation>
-      <Navigation>
-        {nextTutorial && (
-          <Link style={linkStyle} to={`${nextTutorial.route}`}>
-            {nextTutorial.displayName} →
-          </Link>
-        )}
-      </Navigation>
-    </Container>
-  );
+        </Navigation>
+        <Navigation>
+          {nextTutorial && (
+            <Link style={linkStyle} to={`${nextTutorial.route}`}>
+              {nextTutorial.displayName} →
+            </Link>
+          )}
+        </Navigation>
+      </Container>
+    );
 };
 
 export default TutorialNavigation;
